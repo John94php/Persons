6a3cf6f5c83f8c584511ee2741f9e4b21a671854
Johny <janzalesinski@gmail.com>
2021-05-01 18:54:10 +0200

    add all project file

---
+++ src/pkginstanceof/Main.java
@@ -0,0 +1,61 @@
+package pkginstanceof;
+
+public class Main {
+    public static void main(String[] args) {
+        Osoba[] osoba = new Osoba[10];
+        osoba[0] = new Pracownik("Jan", "Zalesiński", 2300);
+        osoba[1] = new Student("Joanna", "Wolff");
+        osoba[0].pobierzOpis();
+        osoba[1].pobierzOpis();
+        for (Osoba person:osoba) {
+            if (person instanceof Osoba) {
+                person.pobierzOpis();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+abstract class Osoba {
+    String imie;
+    String nazwisko;
+
+    Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    abstract void pobierzOpis();
+
+}
+
+class Pracownik extends Osoba {
+    double wynagrodzenie;
+
+    Pracownik(String imie, String nazwisko, double wynagrodzenie) {
+        super(imie, nazwisko);
+        this.wynagrodzenie = wynagrodzenie;
+    }
+
+    @Override
+    void pobierzOpis() {
+        System.out.println("Jestem pracownikiem");
+        System.out.println("Imię " + imie);
+        System.out.println("Nazwisko " + nazwisko);
+        System.out.println("Wynagrodzenie " + wynagrodzenie + "zł");
+    }
+}
+
+class Student extends Osoba {
+    Student(String imie, String nazwisko) {
+        super(imie, nazwisko);
+    }
+
+    @Override
+    void pobierzOpis() {
+        System.out.println("Jestem studentem");
+        System.out.println("Imię " + imie);
+        System.out.println("Nazwisko " + nazwisko);
+    }
+}
